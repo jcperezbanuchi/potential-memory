08b48e7127aa36496884f3ab4f4317ee2d65f141
mrthompson347 <eric.thompson347@gmail.com>
2021-06-11 09:50:06 -0400

    server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World 1');
+});
+
+
+
+app.listen(3000, () => {
+    console.log('Server is up and running on port 3000! :)');
+});
